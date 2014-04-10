aa63fc883f3ea0ffd1dda961914c30a5c1e9d3db
vpmedia <andras@vpmedia.eu>
2014-04-10 21:16:20 +0200

    Cleanup

--- binutils-2.24/ld/testsuite/ld-mips-elf/attr-gnu-4-14.d
+++
@@ -1,5 +0,0 @@
-#source: attr-gnu-4-1.s
-#source: attr-gnu-4-4.s
-#ld: -r
-#warning: Warning: .* uses -mdouble-float \(set by .*\), .* uses -mips32r2 -mfp64
-#target: mips*-*-*
--- binutils-2.24/ld/testsuite/ld-mips-elf/attr-gnu-4-41.d
+++
@@ -1,5 +0,0 @@
-#source: attr-gnu-4-4.s
-#source: attr-gnu-4-1.s
-#ld: -r
-#warning: Warning: .* uses -mips32r2 -mfp64 \(set by .*\), .* uses -mdouble-float
-#target: mips*-*-*
