a94b2ed5ab7065d7be004c6e7243cb49bfc362c9
vpmedia <andras@vpmedia.eu>
2014-06-09 11:31:54 +0200

    Testing LibCo

--- samples/Example_libco/libco_v16/libco.c
+++ samples/Example_libco/libco_v16/libco.c
@@ -20,7 +20,7 @@ int main()
 } 
 
 #if defined(__AVM2__)
-  #include "x86.c"
+  #include "ppc.c"
 #elif defined(__GNUC__) && defined(__i386__)
   #include "x86.c"
 #elif defined(__GNUC__) && defined(__amd64__)
